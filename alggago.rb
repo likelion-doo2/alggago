c6bf5879bb5274c3326865bf6e015e5b475b2636
Likelion <edig292@naver.com>
2016-03-29 11:22:08 +0900

    흰돌/검은돌 이미지 변경

--- alggago.rb
+++ alggago.rb
@@ -1,14 +1,16 @@
 # Encoding: UTF-8
-require 'singleton'
 require 'gosu'
 require 'chipmunk'
-require 'rmagick'
+require 'singleton'
 
 WIDTH, HEIGHT = 1000, 700
 TICK = 1.0/60.0
 NUM_STONES = 20
 PLAYER_COLOR = ["black", "white"]
-FRICTION_FACTOR = 1.50
+STONE_DIAMETER = 50
+RESTITUTION = 0.9
+BOARD_FRICTION = 1.50
+STONE_FRICTION = 0.5
 
 # Layering of sprites
 module ZOrder
@@ -89,27 +91,34 @@ class Stone
 
     @body = CP::Body.new(1, CP::moment_for_circle(1.0, 0, 1, CP::Vec2.new(0, 0))) 
     @body.p = CP::Vec2.new(rand(HEIGHT), rand(HEIGHT)) 
-    @body.v = CP::Vec2.new(rand(500)-250, rand(500)-250)
+    @body.v = CP::Vec2.new(rand(HEIGHT)-HEIGHT/2, rand(HEIGHT)-HEIGHT/2)
 
-    @shape = CP::Shape::Circle.new(body, 23, CP::Vec2.new(0, 0))
-    @shape.e = 0.9
+    @shape = CP::Shape::Circle.new(body, STONE_DIAMETER/2.0, CP::Vec2.new(0, 0))
+    @shape.e = RESTITUTION
+    @shape.u = STONE_FRICTION
 
     @image_body = Gosu::Image.new("media/#{@color}_stone_likelion.png", :tileable => true)
   end
 
   def update
-    new_vel_x = 0.0
-    new_vel_y = 0.0
+    new_vel_x, new_vel_y = 0.0, 0.0
     if @body.v.x != 0 and @body.v.y != 0
-      new_vel_x = (@body.v.x.abs / @body.v.x) * (@body.v.x.abs - FRICTION_FACTOR * (@body.v.x.abs / @body.v.length))
-      new_vel_y = (@body.v.y.abs / @body.v.y) * (@body.v.y.abs - FRICTION_FACTOR * (@body.v.y.abs / @body.v.length))
+      new_vel_x = get_reduced_velocity(@body.v.x, @body.v.length)
+      new_vel_y = get_reduced_velocity(@body.v.y, @body.v.length)
     end
     @body.v = CP::Vec2.new(new_vel_x, new_vel_y)
   end
 
   def draw
-    @image_body.draw_rot(@body.p.x, @body.p.y, ZOrder::Stone, @body.a.radians_to_gosu, 0.5, 0.5, 0.23, 0.23)
+    @image_body.draw_rot(@body.p.x, @body.p.y, ZOrder::Stone, 
+                          @body.a.radians_to_gosu, 0.5, 0.5, STONE_DIAMETER/@image_body.width.to_f, STONE_DIAMETER/@image_body.height.to_f)
   end 
+
+  private
+  def get_reduced_velocity original_velocity, original_velocity_length
+    (original_velocity.abs / original_velocity) * 
+      (original_velocity.abs - BOARD_FRICTION * (original_velocity.abs / original_velocity_length))
+  end
 end
 
 window = Alggago.new
